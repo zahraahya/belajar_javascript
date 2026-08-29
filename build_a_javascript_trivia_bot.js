b1a07de43f641ab56dabe3d0852d7c95c7fe1f16
Zahra Ahya <zahraasuhaila@gmail.com>
2026-08-29 22:32:34 +0700

    Add files via upload

---
+++ build_a_javascript_trivia_bot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!")
+
+let botName= "Zahra";
+let botLocation= "Semarang City";
+let favoriteLanguage="Javascript";
+
+console.log("My name is "+ botName+ " and I live on " + botLocation+ ".")
+console.log ("My favorite programming language is "+favoriteLanguage+".")
+
+let codingFact = favoriteLanguage+ " is awesome, so easy and have a dynamic data types";
+console.log(codingFact);
+
+codingFact= favoriteLanguage+ " is multifunction and can write anywhere.";
+console.log(codingFact);
+
+codingFact= favoriteLanguage+ " is so easy and fast to create.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from " + botLocation+".")
